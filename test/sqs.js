8c1b0d9a23a034c680f6d681364e7bbfedb86943
Randy Li <randy@takescoop.com>
2019-12-02 14:27:38 -0500

    Test that demonstrates sqs-consumer not emitting events

--- test/sqs.js
+++ test/sqs.js
@@ -1,23 +1,48 @@
 'use strict'
 
+const util = require('util')
 const Promise = require('bluebird')
 
 process.env.AWS_ACCESS_KEY_ID = '1'
 process.env.AWS_SECRET_ACCESS_KEY = 'supersecret'
 process.env.AWS_SQS_QUEUE_URL = 'http://localhost:9324/queue/default'
 const sqs = require('../src/sqs')
+const sinon = require('sinon')
+const { Consumer } = require('sqs-consumer')
+
+const setTimeoutPromise = util.promisify(setTimeout)
 
 describe('sqs', function() {
-    
-    beforeEach(async() => {
-        await sqs.purgeQueue({
-            QueueUrl: queueUrl
-        }).promise()
+    afterEach(async function() {
+        sinon.restore()
     })
     
     const queueUrl = process.env.AWS_SQS_QUEUE_URL
-    it('should receive a message', async function() {
+    
+    it('should start and stop the consumer', async function() {
+        const consumer = Consumer.create({
+            sqs: sqs,
+            queueUrl: queueUrl,
+            handleMessage: function() {},
+            waitTimeSeconds: 1,
+            pollingWaitTimeMs: 50,
+            handleMessageTimeout: 1000
+        })
+        await consumer.start()
+        await consumer.stop()
+    })
+    
+    it('should get message_received event', async function() {
         this.timeout(10000)
+        const consumer = Consumer.create({
+            sqs: sqs,
+            queueUrl: queueUrl,
+            handleMessage: function() {},
+            waitTimeSeconds: 1,
+            pollingWaitTimeMs: 50,
+            handleMessageTimeout: 1000
+        })
+        
         await sqs.sendMessage({
             QueueUrl: queueUrl,
             MessageBody: 'Hello queue',
@@ -29,15 +54,14 @@ describe('sqs', function() {
             },
         }).promise()
         
-        const { Messages: messages } = await sqs.receiveMessage({
-            QueueUrl: queueUrl,
-            WaitTimeSeconds: 5
-        }).promise()
+        const tester = { test: () => {} }
+        const messageReceivedSpy = sinon.spy(tester, 'test')
+        consumer.on('message_received', tester.test)
+        await consumer.start()
         
-        expect(messages).to.be.an.array().and.have.length(1)
-        const message = messages[0]
-        expect(message).to.include({
-            Body: 'Hello queue'
-        })
+        await setTimeoutPromise(1200)
+        expect(messageReceivedSpy.called).to.be.true()
+        
+        await consumer.stop()
     })
 })
